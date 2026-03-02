c442cc491e23518c7714c6454c036f69c7e0a8b0
haydenhillis <hh55983@email.vccs.edu>
2026-03-02 18:28:19 -0500

    Add project files.

---
+++ Chapter2Problem1.cpp
@@ -0,0 +1,23 @@
+// Chapter2Problem1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    double oceanlevel = 0;
+    double rise = 1.5;
+    double oceanlevel5years = rise * 5;
+    double oceanlevel7years = rise * 7;
+    double oceanlevel10years = rise * 10;
+    
+    cout << "Ocean level in 5 years: " << oceanlevel5years << " mm" << endl;
+    cout << "Ocean level in 7 years: " << oceanlevel7years << " mm" << endl;
+    cout << "Ocean level in 10 years: " << oceanlevel10years << " mm" << endl;
+
+
+    
+}
+
